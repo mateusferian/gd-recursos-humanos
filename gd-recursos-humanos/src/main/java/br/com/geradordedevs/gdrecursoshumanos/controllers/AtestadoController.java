1deefe04c67d6e34e48397791840118c35c895e2
MateusFerian <mateusferian10@gmail.com>
2022-11-25 10:07:06 -0300

    :recycle: refactor: ajustando mapeamento de AtestadoController no RequestMapping para minusculo

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/controllers/AtestadoController.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/controllers/AtestadoController.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Atestados")
+@RequestMapping("/atestados")
 public class AtestadoController {
 
           @Autowired
