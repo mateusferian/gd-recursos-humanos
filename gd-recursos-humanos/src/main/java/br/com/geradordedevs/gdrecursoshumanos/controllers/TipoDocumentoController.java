07b9fc99822c13c82fcb428a8e1e6000558253fb
MateusFerian <mateusferian10@gmail.com>
2022-11-25 10:16:43 -0300

    :recycle: refactor: ajustando mapeamento de TipoDocumentoController no RequestMapping para minusculo

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/controllers/TipoDocumentoController.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/controllers/TipoDocumentoController.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping ("/tiposdocumentos")
+@RequestMapping ("/tipo-de-documentos")
 public class TipoDocumentoController {
 
     @GetMapping
