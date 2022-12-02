4fb54ed13279cb2e69ed3bf85cd4e3ae8c15e2a5
MateusFerian <mateusferian10@gmail.com>
2022-12-01 23:50:34 -0300

    :sparkles: feat: fazendo o relacionamento em AtestadoEntity

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/entities/AtestadoEntity.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/entities/AtestadoEntity.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 @Entity
 @Data
@@ -19,6 +16,8 @@ public class AtestadoEntity {
     private Long id;
     private String nome;
     private Date data;
+    @ManyToOne
+    private ColaboradorEntity colaborador;
 }
 
 
