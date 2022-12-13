70020118b99cdcce62f73b2077a15bea231e9858
MateusFerian <mateusferian10@gmail.com>
2022-12-13 12:37:08 -0300

    :sparkles: feat: Criando a response CargoResponseDTO na camada de dtos

---
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/responses/CargoResponseDTO.java
@@ -0,0 +1,14 @@
+package br.com.geradordedevs.gdrecursoshumanos.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CargoResponseDTO {
+
+    private Long id;
+    private String nome;
+}
