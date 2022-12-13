0d2c30e23b933be6c9ecb894f6655867dff3ce93
MateusFerian <mateusferian10@gmail.com>
2022-12-13 16:16:48 -0300

    :sparkles: feat: Adicionando logs em CargoMapper

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/mapper/CargoMapper.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/mapper/CargoMapper.java
@@ -4,6 +4,7 @@ import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.CargoRequestDTO;
 import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.CargoResponseDTO;
 import br.com.geradordedevs.gdrecursoshumanos.entities.CargoEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,17 +14,21 @@ import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CargoMapper {
     @Autowired
     private final ModelMapper mapper;
     public CargoResponseDTO paraDto(CargoEntity entidade){
+        log.info("convertendo entidade {} para Dto", entidade);
         return  mapper.map(entidade, CargoResponseDTO.class);
     }
     public CargoEntity paraEntidade(CargoRequestDTO request){
+        log.info("convertendo dto {} para entidade", request);
         return  mapper.map(request, CargoEntity.class);
 
     }
     public List<CargoResponseDTO> paraListaDto(List<CargoEntity> lista){
+        log.info("convertendo lista de entidade {} para lista de dto", lista);
         return  lista.stream()
                 .map(this::paraDto)
                 .collect(Collectors.toList());
