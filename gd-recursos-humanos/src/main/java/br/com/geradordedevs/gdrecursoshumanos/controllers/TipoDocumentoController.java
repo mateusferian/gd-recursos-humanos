03ea01e98e3302bc88cc3c112c2bc491f04dde27
MateusFerian <mateusferian10@gmail.com>
2022-12-13 14:25:12 -0300

    :recycle: refactor: Ajustando TipoDocumentoController expondo DTO ao invés da Entity

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/controllers/TipoDocumentoController.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/controllers/TipoDocumentoController.java
@@ -1,5 +1,7 @@
 package br.com.geradordedevs.gdrecursoshumanos.controllers;
 
+import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.TipoDocumentoRequestDTO;
+import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.TipoDocumentoResponseDTO;
 import br.com.geradordedevs.gdrecursoshumanos.entities.ColaboradorEntity;
 import br.com.geradordedevs.gdrecursoshumanos.entities.TipoDocumentoEntity;
 import br.com.geradordedevs.gdrecursoshumanos.repositories.TipoDocumentoRepository;
@@ -19,23 +21,23 @@ public class TipoDocumentoController {
     private TipoDocumentoService tipoDocumentoService;
 
     @GetMapping
-    public Iterable<TipoDocumentoEntity> listar(){
+    public List<TipoDocumentoResponseDTO> listar(){
         return tipoDocumentoService.listar();
     }
 
     @GetMapping ("/{id}")
-    public TipoDocumentoEntity consultar(@PathVariable Long id ){
+    public TipoDocumentoResponseDTO consultar(@PathVariable Long id ){
         return tipoDocumentoService.consultar(id);
     }
 
     @PostMapping
-    public TipoDocumentoEntity cadastrar(@RequestBody TipoDocumentoEntity tipoDocumentoEntity){
-        return tipoDocumentoService.cadastrar(tipoDocumentoEntity);
+    public TipoDocumentoResponseDTO cadastrar(@RequestBody TipoDocumentoRequestDTO request){
+        return tipoDocumentoService.cadastrar(request);
     }
 
     @PutMapping ("/{id}")
-    public TipoDocumentoEntity alterar (@PathVariable Long id, @RequestBody TipoDocumentoEntity tipoDocumentoEntity){
-        return tipoDocumentoService.alterar(id, tipoDocumentoEntity);
+    public TipoDocumentoResponseDTO alterar (@PathVariable Long id, @RequestBody TipoDocumentoRequestDTO request){
+        return tipoDocumentoService.alterar(id, request);
     }
 
     @DeleteMapping ("/{id}")
@@ -44,7 +46,7 @@ public class TipoDocumentoController {
     }
 
     @GetMapping("/popular")
-    public Iterable<TipoDocumentoEntity> popularBanco(){
+    public List<TipoDocumentoResponseDTO> popularBanco(){
         tipoDocumentoService.popular();
         return tipoDocumentoService.listar();
     }
