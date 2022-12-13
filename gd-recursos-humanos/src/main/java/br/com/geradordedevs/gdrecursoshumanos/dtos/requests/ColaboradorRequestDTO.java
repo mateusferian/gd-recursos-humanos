b4d94c4e5888eafd0482586fba362762713e1d0c
MateusFerian <mateusferian10@gmail.com>
2022-12-13 15:10:23 -0300

    :sparkles: feat: Criando a request ColaboradorRequestDTO na camada de dtos

---
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/requests/ColaboradorRequestDTO.java
@@ -0,0 +1,27 @@
+package br.com.geradordedevs.gdrecursoshumanos.dtos.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ColaboradorRequestDTO {
+
+    private Long id;
+    private String nome;
+    private int idade;
+    private Date dataDeNascimento;
+    private Long tipoDucumentoId;
+    private String numeroDocumento;
+    private Long cargoId;
+    private Long departamentoId;
+    private double salario;
+    private Date dataInicio;
+    private boolean ativo;
+    private String telefone;
+    private String email;
+}
