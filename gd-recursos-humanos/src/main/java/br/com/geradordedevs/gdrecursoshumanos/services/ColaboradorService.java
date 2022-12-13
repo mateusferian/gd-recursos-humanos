95654105f75bf95026de284ab5a067db6a7ca5d3
MateusFerian <mateusferian10@gmail.com>
2022-12-13 15:16:04 -0300

    :recycle: refactor: Ajustando ColaboradorService

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/services/ColaboradorService.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/services/ColaboradorService.java
@@ -1,14 +1,18 @@
 package br.com.geradordedevs.gdrecursoshumanos.services;
 
+import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.ColaboradorRequestDTO;
+import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.ColaboradorResponseDTO;
 import br.com.geradordedevs.gdrecursoshumanos.entities.ColaboradorEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 public interface ColaboradorService {
-    Iterable<ColaboradorEntity> listar();
-    ColaboradorEntity consultar(@PathVariable Long id );
-    ColaboradorEntity cadastrar(@RequestBody ColaboradorEntity colaboradorEntity);
-    ColaboradorEntity alterar (@PathVariable Long id, @RequestBody ColaboradorEntity colaboradorEntity);
-    void remover (@PathVariable Long id);
+    List<ColaboradorResponseDTO> listar();
+    ColaboradorResponseDTO consultar(Long id );
+    ColaboradorResponseDTO cadastrar( ColaboradorRequestDTO request);
+    ColaboradorResponseDTO alterar ( Long id,  ColaboradorRequestDTO request);
+    void remover ( Long id);
      void popular();
 }
