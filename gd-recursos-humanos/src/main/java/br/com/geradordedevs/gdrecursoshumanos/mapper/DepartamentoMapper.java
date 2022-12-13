899c603835a79d20180a416c05adde295953c728
MateusFerian <mateusferian10@gmail.com>
2022-12-13 16:16:15 -0300

    :sparkles: feat: Adicionando logs em DepartamentoMapper

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/mapper/DepartamentoMapper.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/mapper/DepartamentoMapper.java
@@ -5,6 +5,7 @@ import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.DepartamentoRequestD
 import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.DepartamentoResponseDTO;
 import br.com.geradordedevs.gdrecursoshumanos.entities.DepartamentoEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,17 +14,21 @@ import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DepartamentoMapper {
     @Autowired
     private final ModelMapper mapper;
     public DepartamentoResponseDTO paraDto(DepartamentoEntity entidade){
+        log.info("convertendo entidade {} para Dto", entidade);
         return  mapper.map(entidade, DepartamentoResponseDTO.class);
     }
     public DepartamentoEntity paraEntidade(DepartamentoRequestDTO request){
+        log.info("convertendo dto {} para entidade", request);
         return  mapper.map(request, DepartamentoEntity.class);
 
     }
     public List<DepartamentoResponseDTO> paraListaDto(List<DepartamentoEntity> lista){
+        log.info("convertendo lista de entidade {} para lista de dto", lista);
         return  lista.stream()
                 .map(this::paraDto)
                 .collect(Collectors.toList());
