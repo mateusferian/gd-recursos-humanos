3d18617914396b2d1d18d513ff25207b7f714225
MateusFerian <mateusferian10@gmail.com>
2022-12-13 15:14:13 -0300

    :recycle: refactor: Removendo PathVariable do metodo remover de DepartamentoService

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/services/DepartamentoService.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/services/DepartamentoService.java
@@ -14,6 +14,6 @@ public interface DepartamentoService {
     DepartamentoResponseDTO consultar(Long id);
     DepartamentoResponseDTO cadastrar(DepartamentoRequestDTO request);
     DepartamentoResponseDTO alterar(Long id, DepartamentoRequestDTO request);
-    void remover(@PathVariable Long id);
+    void remover( Long id);
      void popular();
 }
