5f39cbfbeb5d7d6ddb2d054bc5e4b8eeed4987df
MateusFerian <mateusferian10@gmail.com>
2022-12-13 23:24:30 -0300

    :sparkles: feat: Adicionando as dependencias de AtestadoMapper em AtestadoServiceImpl

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/services/impl/AtestadoServiceImpl.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/services/impl/AtestadoServiceImpl.java
@@ -1,42 +1,56 @@
 package br.com.geradordedevs.gdrecursoshumanos.services.impl;
 
+import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.AtestadoRequestDTO;
+import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.AtestadoResponseDTO;
 import br.com.geradordedevs.gdrecursoshumanos.entities.AtestadoEntity;
 import br.com.geradordedevs.gdrecursoshumanos.entities.CargoEntity;
 import br.com.geradordedevs.gdrecursoshumanos.entities.ColaboradorEntity;
+import br.com.geradordedevs.gdrecursoshumanos.mapper.AtestadoMapper;
 import br.com.geradordedevs.gdrecursoshumanos.repositories.AtestadoRepository;
 import br.com.geradordedevs.gdrecursoshumanos.services.AtestadoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @Slf4j
 public class AtestadoServiceImpl implements AtestadoService {
     @Autowired
     public AtestadoRepository atestadoRepository;
+    @Autowired
+    public AtestadoMapper mapper;
 
     @Override
-    public Iterable<AtestadoEntity> listar() {
+    public List<AtestadoResponseDTO> listar() {
         log.info("listando atestados");
-        return atestadoRepository.findAll();
+        List<AtestadoEntity> atestadoEntities = new ArrayList<>();
+        for (AtestadoEntity atestadoEntity: atestadoRepository.findAll()) {
+            atestadoEntities.add(atestadoEntity);
+        }
+        return mapper.paraListaDto(atestadoEntities);
     }
     @Override
-    public AtestadoEntity consultar(Long id) {
+    public AtestadoResponseDTO consultar(Long id) {
         log.info("obtendo informações de atestado {}", id);
-        return atestadoRepository.findById(id).orElse(new AtestadoEntity());
+        return mapper.paraDto(atestadoRepository.findById(id).orElse(new AtestadoEntity()));
     }
     @Override
-    public AtestadoEntity cadastrar(AtestadoEntity atestadoEntity) {
-        log.info("cadastrando um novo atestado {}", atestadoEntity);
-        return  atestadoRepository.save(atestadoEntity);
+    public AtestadoResponseDTO cadastrar(AtestadoRequestDTO request) {
+        log.info("cadastrando um novo atestado {}", request);
+        return  mapper.paraDto(atestadoRepository.save(mapper.paraEntidade(request)));
     }
     @Override
-    public AtestadoEntity alterar(long id, AtestadoEntity atestadoEntity) {
-        log.info("alterando o atestado de id {} com novas informações: {}", id, atestadoEntity);
-        atestadoEntity.setId(id);
-        return atestadoRepository.save(atestadoEntity);
+    public AtestadoResponseDTO alterar(long id, AtestadoRequestDTO request) {
+        log.info("alterando o atestado de id {} com novas informações: {}", id, request);
+        AtestadoEntity atestado = mapper.paraEntidade(request);
+        atestado.setId(id);
+
+
+        return mapper.paraDto(atestadoRepository.save(atestado));
     }
     @Override
     public void remover(Long id) {
