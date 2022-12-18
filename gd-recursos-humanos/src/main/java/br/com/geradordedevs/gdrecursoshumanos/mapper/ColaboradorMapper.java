b5f6b81da31efbe831573610d89a54c4a1ce7b9f
Mateus Ferian <mateusferian10@gmail.com>
2022-12-18 10:29:27 -0300

    :recycle: refactor: Ajustando ColaboradorMapper

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/mapper/ColaboradorMapper.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/mapper/ColaboradorMapper.java
@@ -26,35 +26,14 @@ import java.util.stream.Collectors;
 public class ColaboradorMapper {
     @Autowired
     private final ModelMapper mapper;
-    @Autowired
-    private CargoRepository cargoRepository;
-    @Autowired
-    private DepartamentoRepository departamentoRepository;
-    @Autowired
-    private TipoDocumentoRepository tipoDocumentoRepository;
+
     public ColaboradorResponseDTO paraDto(ColaboradorEntity entidade){
         log.info("convertendo entidade {} para dto", entidade);
-        ColaboradorResponseDTO colaboradorResponseDTO = new ColaboradorResponseDTO();
-        colaboradorResponseDTO = mapper.map(entidade, ColaboradorResponseDTO.class);
-        return colaboradorResponseDTO;
+        return  mapper.map(entidade, ColaboradorResponseDTO.class);
     }
     public ColaboradorEntity  paraEntidade(ColaboradorRequestDTO request){
         log.info("convertendo dto {} para entidade", request);
-        ColaboradorEntity  colaboradorEntity = mapper.map(request, ColaboradorEntity.class);
-
-        CargoEntity cargoEntity = cargoRepository.findById(request.getCargoId()).get();
-
-        colaboradorEntity.setCargo(cargoEntity);
-
-        DepartamentoEntity departamento = departamentoRepository.findById(request.getDepartamentoId()).get();
-
-        colaboradorEntity.setDepartamento(departamento);
-
-        TipoDocumentoEntity tipoDocumentoEntity = tipoDocumentoRepository.findById(request.getTipoDucumentoId()).get();
-
-        colaboradorEntity.setTipoDucumento(tipoDocumentoEntity);
-
-        return colaboradorEntity;
+        return  mapper.map(request, ColaboradorEntity.class);
     }
     public List<ColaboradorResponseDTO> paraListaDto(List<ColaboradorEntity > lista){
         log.info("convertendo lista de entidade {} para lista de dto", lista);
