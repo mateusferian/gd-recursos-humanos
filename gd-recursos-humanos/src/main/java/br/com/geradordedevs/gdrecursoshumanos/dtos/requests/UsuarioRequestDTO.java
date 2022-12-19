cef1236067d495e2be871b31ba7bb307e012d1db
Mateus Ferian <mateusferian10@gmail.com>
2022-12-19 18:24:45 -0300

    :sparkles: feat: Criando UsuarioRequestDTO

---
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/requests/UsuarioRequestDTO.java
@@ -0,0 +1,15 @@
+package br.com.geradordedevs.gdrecursoshumanos.dtos.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioRequestDTO {
+
+    private String nome;
+    private String email;
+    private String senha;
+}
