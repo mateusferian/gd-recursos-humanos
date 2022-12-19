9592eca3c2bd65e0f08171d385cdd5e0148c5470
Mateus Ferian <mateusferian10@gmail.com>
2022-12-19 10:02:06 -0300

    :sparkles: feat: Adicionando id em ColaboradorRequestDTO

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/requests/ColaboradorRequestDTO.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/requests/ColaboradorRequestDTO.java
@@ -10,7 +10,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ColaboradorRequestDTO {
-
+    private Long id;
     private String nome;
     private int idade;
     private Date dataDeNascimento;
