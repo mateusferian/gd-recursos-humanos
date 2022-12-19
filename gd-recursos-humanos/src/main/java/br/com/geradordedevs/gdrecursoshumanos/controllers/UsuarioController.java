118519a2884c98b048670501e9cdd08822af6110
Mateus Ferian <mateusferian10@gmail.com>
2022-12-19 18:32:41 -0300

    :sparkles: feat: Criando UsuarioController

---
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/controllers/UsuarioController.java
@@ -0,0 +1,42 @@
+package br.com.geradordedevs.gdrecursoshumanos.controllers;
+
+import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.UsuarioRequestDTO;
+import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.UsuarioResponseDTO;
+import br.com.geradordedevs.gdrecursoshumanos.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping
+    public List<UsuarioResponseDTO> listar(){
+        return usuarioService.listar();
+    }
+
+    @GetMapping ("/{id}")
+    public UsuarioResponseDTO consultar(@PathVariable Long id){
+        return  usuarioService.consultar(id);
+    }
+    
+    @PostMapping
+    public  UsuarioResponseDTO cadastrar(@RequestBody UsuarioRequestDTO request){
+        return  usuarioService.cadastrar(request);
+    }
+
+    @PutMapping ("/{id}")
+    public  UsuarioResponseDTO alterar(@PathVariable Long id, @RequestBody UsuarioRequestDTO request){
+        return  usuarioService.alterar(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public  void  remover(@PathVariable Long id){
+        usuarioService.remover(id);
+    }
+}
