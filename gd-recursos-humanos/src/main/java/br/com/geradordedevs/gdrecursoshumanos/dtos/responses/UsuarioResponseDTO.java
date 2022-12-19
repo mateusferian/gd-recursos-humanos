739abba2bf84df57340b04759481b006a203cd33
Mateus Ferian <mateusferian10@gmail.com>
2022-12-19 18:24:22 -0300

    :sparkles: feat: Criando UsuarioResponseDTO

---
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/responses/UsuarioResponseDTO.java
@@ -0,0 +1,16 @@
+package br.com.geradordedevs.gdrecursoshumanos.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioResponseDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+}
