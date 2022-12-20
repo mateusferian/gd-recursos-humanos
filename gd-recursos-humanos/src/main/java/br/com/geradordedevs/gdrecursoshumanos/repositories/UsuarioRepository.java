507defc3b3c9aaaa874bebcf1a42e67ad61e8beb
Mateus Ferian <mateusferian10@gmail.com>
2022-12-19 21:15:51 -0300

    :sparkles: feat: Adicionando personalização em UsuarioRepository

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/repositories/UsuarioRepository.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/repositories/UsuarioRepository.java
@@ -4,4 +4,5 @@ import br.com.geradordedevs.gdrecursoshumanos.entities.UsuarioEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository<UsuarioEntity, Long> {
+    UsuarioEntity findByEmail(String email);
 }
