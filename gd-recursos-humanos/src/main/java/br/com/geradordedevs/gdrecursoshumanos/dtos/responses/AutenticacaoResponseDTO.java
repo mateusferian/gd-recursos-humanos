9a6caf5c6ef3363093d830ca2f786493321e1a6c
Mateus Ferian <mateusferian10@gmail.com>
2022-12-19 21:05:25 -0300

    :sparkles: feat: Criando AutenticacaoResponseDTO

---
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/responses/AutenticacaoResponseDTO.java
@@ -0,0 +1,12 @@
+package br.com.geradordedevs.gdrecursoshumanos.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AutenticacaoResponseDTO {
+    private  String token;
+}
