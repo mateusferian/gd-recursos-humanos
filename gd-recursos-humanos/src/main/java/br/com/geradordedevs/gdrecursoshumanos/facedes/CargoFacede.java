dabb35434b5e29267c5efa29f6aa5d1dd6baf320
Mateus Ferian <mateusferian10@gmail.com>
2022-12-20 23:52:51 -0300

    :sparkles: feat: Criando cada Facede na camada facedes

---
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/facedes/CargoFacede.java
@@ -0,0 +1,17 @@
+package br.com.geradordedevs.gdrecursoshumanos.facedes;
+
+import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.CargoRequestDTO;
+import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.ColaboradorRequestDTO;
+import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.CargoResponseDTO;
+import br.com.geradordedevs.gdrecursoshumanos.entities.CargoEntity;
+
+import java.util.List;
+
+public interface CargoFacede {
+    List<CargoResponseDTO> listar();
+    CargoResponseDTO consultar(Long id);
+    CargoResponseDTO cadastrar(CargoRequestDTO request);
+    CargoResponseDTO alterar(Long id, CargoRequestDTO request);
+    void remover(Long id);
+    void popular();
+}
