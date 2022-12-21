98b7e12a2dbd27449b220a2dd81de93c15c8d902
Mateus Ferian <mateusferian10@gmail.com>
2022-12-21 13:49:57 -0300

    :bug: fix: Arrumando erro de ficar null nos relacionamentos ao cadastrar um novo colaborador

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/mappers/ColaboradorMapper.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/mappers/ColaboradorMapper.java
@@ -2,7 +2,13 @@ package br.com.geradordedevs.gdrecursoshumanos.mappers;
 
 import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.ColaboradorRequestDTO;
 import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.ColaboradorResponseDTO;
+import br.com.geradordedevs.gdrecursoshumanos.entities.CargoEntity;
 import br.com.geradordedevs.gdrecursoshumanos.entities.ColaboradorEntity;
+import br.com.geradordedevs.gdrecursoshumanos.entities.DepartamentoEntity;
+import br.com.geradordedevs.gdrecursoshumanos.entities.TipoDocumentoEntity;
+import br.com.geradordedevs.gdrecursoshumanos.repositories.CargoRepository;
+import br.com.geradordedevs.gdrecursoshumanos.repositories.DepartamentoRepository;
+import br.com.geradordedevs.gdrecursoshumanos.repositories.TipoDocumentoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -21,6 +27,15 @@ public class ColaboradorMapper {
     @Autowired
     private final ModelMapper mapper;
 
+    @Autowired
+    private DepartamentoRepository departamentoRepository;
+
+    @Autowired
+    private CargoRepository cargoRepository;
+
+    @Autowired
+    private TipoDocumentoRepository tipoDocumentoRepository;
+
     public ColaboradorResponseDTO paraDto(ColaboradorEntity entidade){
         log.info("convertendo entidade {} para dto", entidade);
         return  mapper.map(entidade, ColaboradorResponseDTO.class);
@@ -28,7 +43,14 @@ public class ColaboradorMapper {
 
     public ColaboradorEntity  paraEntidade(ColaboradorRequestDTO request){
         log.info("convertendo dto {} para entidade", request);
-        return  mapper.map(request, ColaboradorEntity.class);
+
+        ColaboradorEntity colaboradorEntity =mapper.map(request,ColaboradorEntity.class);
+        colaboradorEntity.setDepartamento(departamentoRepository.findById(request.getDepartamento()).orElse(new DepartamentoEntity()));
+        colaboradorEntity.setCargo(cargoRepository.findById(request.getCargo()).orElse(new CargoEntity()));
+        colaboradorEntity.setTipoDocumento(tipoDocumentoRepository.findById(request.getTipoDocumento()).orElse(new TipoDocumentoEntity()));
+        colaboradorEntity.setDepartamento(departamentoRepository.findById(request.getDepartamento()).orElse(new DepartamentoEntity()));
+
+        return colaboradorEntity;
     }
 
     public List<ColaboradorResponseDTO> paraListaDto(Iterable<ColaboradorEntity > lista){
