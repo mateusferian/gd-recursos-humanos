7916e014e1120b2ff9c55845dcccb1b5bba7e5a6
Mateus Ferian <mateusferian10@gmail.com>
2022-12-22 15:45:21 -0300

    :sparkles: feat: Adicionando novas validações em UsuarioRequestDTO

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/requests/UsuarioRequestDTO.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/requests/UsuarioRequestDTO.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
@@ -17,6 +18,8 @@ public class UsuarioRequestDTO {
     @Size(min = 3,max = 40, message = "{tamanho.invalido}")
     private String nome;
 
+    @NotNull(message = "{campo.nulo}")
+    @Size(min = 3,max = 40, message = "{tamanho.invalido}")
     @Email(message = "{email.invalido}")
     private String email;
 
