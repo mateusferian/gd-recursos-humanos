84d5c839ae10039b262b0a08125f0e09772e8aaa
Mateus Ferian <mateusferian10@gmail.com>
2022-12-22 15:45:53 -0300

    :sparkles: feat: Adicionando novas validações em ColaboradorRequestDTO

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/requests/ColaboradorRequestDTO.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/requests/ColaboradorRequestDTO.java
@@ -20,7 +20,7 @@ public class ColaboradorRequestDTO {
     @Min(value = 18, message = "{idade.abaixo.do.minimo}")
     @Max(value = 70, message = "{idade.acima.do.maximo}")
     private int idade;
-
+    @Past(message ="{data.nascimento}")
     @NotNull(message = "{campo.nulo}")
     private Date dataDeNascimento;
 
@@ -28,6 +28,7 @@ public class ColaboradorRequestDTO {
     @Min(value = 1, message = "{idade.abaixo.do.minimo}")
     private Long tipoDocumento;
 
+    @NotBlank(message = "{campo.em.branco}")
     @Size(min = 10,max = 20, message = "{tamanho.invalido}")
     private String numeroDocumento;
 
@@ -37,20 +38,26 @@ public class ColaboradorRequestDTO {
 
     @NotNull(message = "{campo.nulo}")
     @Min(value = 1, message = "{id.abaixo.do.minimo}")
+    @Max(value = 70, message = "{idade.acima.do.maximo}")
     private Long departamento;
 
     @Min(value = 1000, message = "{salario.abaixo.do.minimo}")
+    @Max(value = 30000, message = "{salario.acima.do.maximo}")
     private double salario;
 
+    @PastOrPresent(message = "{data.inicio.invalida}")
     @NotNull(message = "{campo.nulo}")
     private Date dataInicio;
 
     @NotNull(message = "{campo.nulo}")
     private boolean ativo;
 
+    @NotBlank(message = "{campo.em.branco}")
     @Size(min = 8,max = 15, message = "{tamanho.invalido}")
     private String telefone;
 
+    @NotNull(message = "{campo.nulo}")
+    @Size(min = 3,max = 40, message = "{tamanho.invalido}")
     @Email(message = "{email.invalido}")
     private String email;
 }
