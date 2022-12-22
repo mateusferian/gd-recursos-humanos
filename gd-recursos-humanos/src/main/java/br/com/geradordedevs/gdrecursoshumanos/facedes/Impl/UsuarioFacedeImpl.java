5411c23cbfaf574d7a1bb3c3780963dbc3dc7e63
Mateus Ferian <mateusferian10@gmail.com>
2022-12-22 11:24:17 -0300

    :recycle: refactor: Fazendo identação de UsuarioFacedeImpl

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/facedes/Impl/UsuarioFacedeImpl.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/facedes/Impl/UsuarioFacedeImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 @Component
 public class UsuarioFacedeImpl implements UsuarioFacede {
+
     @Autowired
     private UsuarioService usuarioService;
 
