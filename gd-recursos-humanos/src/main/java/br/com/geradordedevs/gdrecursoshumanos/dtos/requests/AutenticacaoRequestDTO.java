335aee1587d09433bfd9d045f643d27c4d4ee41c
Mateus Ferian <mateusferian10@gmail.com>
2022-12-22 15:46:54 -0300

    :sparkles: feat: Adicionando novas validações em AutenticacaoRequestDTO

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/requests/AutenticacaoRequestDTO.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/dtos/requests/AutenticacaoRequestDTO.java
@@ -5,11 +5,14 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @NoArgsConstructor
 @Getter
 public class AutenticacaoRequestDTO {
+    @NotNull(message = "{campo.nulo}")
+    @Size(min = 3,max = 40, message = "{tamanho.invalido}")
     @Email(message = "{email.invalido}")
     private  String email;
 
