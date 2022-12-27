851273036ddb7c97c95982a451925e9923ad2421
Mateus Ferian <mateusferian10@gmail.com>
2022-12-27 02:53:07 -0300

    :bug: fix: Arrumando erro ao cadastrar e popular um novo colaborador

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/facedes/impl/ColaboradorFacedeImpl.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/facedes/impl/ColaboradorFacedeImpl.java
@@ -4,7 +4,10 @@ import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.ColaboradorRequestDT
 import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.ColaboradorResponseDTO;
 import br.com.geradordedevs.gdrecursoshumanos.facedes.ColaboradorFacede;
 import br.com.geradordedevs.gdrecursoshumanos.mappers.ColaboradorMapper;
+import br.com.geradordedevs.gdrecursoshumanos.services.CargoService;
 import br.com.geradordedevs.gdrecursoshumanos.services.ColaboradorService;
+import br.com.geradordedevs.gdrecursoshumanos.services.DepartamentoService;
+import br.com.geradordedevs.gdrecursoshumanos.services.TipoDocumentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +22,15 @@ public class ColaboradorFacedeImpl implements ColaboradorFacede {
     @Autowired
     private ColaboradorService colaboradorService;
 
+    @Autowired
+    private CargoService cargoService;
+
+    @Autowired
+    private DepartamentoService departamentoService;
+
+    @Autowired
+    private TipoDocumentoService tipoDocumentoService;
+
     @Override
     public List<ColaboradorResponseDTO> listar() {
         return mapper.paraListaDto(colaboradorService.listar());
@@ -31,11 +43,21 @@ public class ColaboradorFacedeImpl implements ColaboradorFacede {
 
     @Override
     public ColaboradorResponseDTO cadastrar(ColaboradorRequestDTO request) {
+
+        cargoService.consultar(request.getCargo());
+        departamentoService.consultar(request.getDepartamento());
+        tipoDocumentoService.consultar(request.getTipoDocumento());
+
         return mapper.paraDto(colaboradorService.cadastrar(mapper.paraEntidade(request)));
     }
 
     @Override
     public ColaboradorResponseDTO alterar(Long id, ColaboradorRequestDTO request) {
+
+        cargoService.consultar(request.getCargo());
+        departamentoService.consultar(request.getDepartamento());
+        tipoDocumentoService.consultar(request.getTipoDocumento());
+
         return mapper.paraDto(colaboradorService.alterar(id,mapper.paraEntidade(request)));
     }
 
