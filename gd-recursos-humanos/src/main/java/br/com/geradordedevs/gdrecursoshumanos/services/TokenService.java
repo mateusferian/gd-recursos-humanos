f27baa15347439ed9d9d24663425e9eb49f05e5f
Mateus Ferian <mateusferian10@gmail.com>
2022-12-27 16:59:49 -0300

    :recycle: refactor: Ajustando metodo em TokenService

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/services/TokenService.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/services/TokenService.java
@@ -1,8 +1,6 @@
 package br.com.geradordedevs.gdrecursoshumanos.services;
 
-import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.JwtResponseDTO;
-
 public interface TokenService {
     String gerarToken(String email);
-    JwtResponseDTO validar(String token);
+    void validar(String token);
 }
