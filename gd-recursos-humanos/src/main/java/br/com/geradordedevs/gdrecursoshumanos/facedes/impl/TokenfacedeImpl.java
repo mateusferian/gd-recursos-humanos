ffacbafcc155e7503e983e421eef4454231d9b84
Mateus Ferian <mateusferian10@gmail.com>
2022-12-27 02:24:07 -0300

    :sparkles: feat: Criando TokenFacedeImpl

---
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/facedes/impl/TokenfacedeImpl.java
@@ -0,0 +1,25 @@
+package br.com.geradordedevs.gdrecursoshumanos.facedes.impl;
+
+import br.com.geradordedevs.gdrecursoshumanos.dtos.requests.AutenticacaoRequestDTO;
+import br.com.geradordedevs.gdrecursoshumanos.dtos.responses.AutenticacaoResponseDTO;
+import br.com.geradordedevs.gdrecursoshumanos.facedes.TokenFacede;
+import br.com.geradordedevs.gdrecursoshumanos.services.TokenService;
+import br.com.geradordedevs.gdrecursoshumanos.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenfacedeImpl implements TokenFacede {
+    
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    public AutenticacaoResponseDTO autenticacao(AutenticacaoRequestDTO request) {
+        usuarioService.validarUsuarioSenha(request);
+        return new AutenticacaoResponseDTO(tokenService.gerarToken(request.getEmail()));
+    }
+}
