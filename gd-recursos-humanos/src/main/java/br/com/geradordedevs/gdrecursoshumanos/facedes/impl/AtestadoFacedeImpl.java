2fd3f13ab6c3ede45c7f94b53e04f8371e114bda
Mateus Ferian <mateusferian10@gmail.com>
2022-12-27 13:28:13 -0300

    :recycle: refactor: Ajustando AtestadoFacedeImpl

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/facedes/impl/AtestadoFacedeImpl.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/facedes/impl/AtestadoFacedeImpl.java
@@ -6,6 +6,7 @@ import br.com.geradordedevs.gdrecursoshumanos.facedes.AtestadoFacede;
 import br.com.geradordedevs.gdrecursoshumanos.mappers.AtestadoMapper;
 import br.com.geradordedevs.gdrecursoshumanos.services.AtestadoService;
 import br.com.geradordedevs.gdrecursoshumanos.services.ColaboradorService;
+import br.com.geradordedevs.gdrecursoshumanos.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -22,34 +23,43 @@ public class AtestadoFacedeImpl implements AtestadoFacede {
     @Autowired
     private ColaboradorService colaboradorService;
 
+    @Autowired
+    private TokenService tokenService;
+
     @Override
-    public List<AtestadoResponseDTO> listar() {
+    public List<AtestadoResponseDTO> listar(String token) {
+        tokenService.validar(token);
         return mapper.paraListaDto(atestadoService.listar());
     }
 
     @Override
-    public AtestadoResponseDTO consultar(Long id) {
+    public AtestadoResponseDTO consultar(Long id,String token) {
+        tokenService.validar(token);
         return mapper.paraDto(atestadoService.consultar(id));
     }
 
     @Override
-    public AtestadoResponseDTO cadastrar(AtestadoRequestDTO request) {
+    public AtestadoResponseDTO cadastrar(AtestadoRequestDTO request,String token) {
+        tokenService.validar(token);
         colaboradorService.consultar(request.getColaborador());
         return mapper.paraDto(atestadoService.cadastrar(mapper.paraEntidade(request)));
     }
 
     @Override
-    public AtestadoResponseDTO alterar(Long id, AtestadoRequestDTO request) {
+    public AtestadoResponseDTO alterar(Long id, AtestadoRequestDTO request,String token) {
+        tokenService.validar(token);
         return mapper.paraDto(atestadoService.alterar(id,mapper.paraEntidade(request)));
     }
 
     @Override
-    public void remover(Long id) {
+    public void remover(Long id,String token) {
+        tokenService.validar(token);
         atestadoService.remover(id);
     }
 
     @Override
-    public void popular() {
+    public void popular(String token) {
+        tokenService.validar(token);
         atestadoService.popular();
     }
 }
