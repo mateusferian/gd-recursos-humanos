6d15848288e556d8f7829c21f53ab47647f41be8
Mateus Ferian <mateusferian10@gmail.com>
2022-12-27 17:32:12 -0300

    :recycle: refactor: Ajustando TokenEnum

--- gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/exceptions/enums/TokenEnum.java
+++ gd-recursos-humanos/src/main/java/br/com/geradordedevs/gdrecursoshumanos/exceptions/enums/TokenEnum.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public enum TokenEnum {
     TOKEN_INVALIDO("TOKEN_INVALIDO", "Token inválido", 401),
-    TOKEN_EXPIRADO("TOKEN_EXPIRADO", "Token expirado", 401),
     TOKEN_OBRIGATORIO("TOKEN_OBRIGATORIO", "Token obrigatório", 401);
 
     private String code;
